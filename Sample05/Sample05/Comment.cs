5c8a95b2e0655114ae666d8284a94f86f0ef8e31
qingchangxie <29313020@qq.com>
2022-06-05 11:23:02 +0800

    Sample05 add

---
+++ Sample05/Sample05/Comment.cs
@@ -0,0 +1,22 @@
+﻿namespace Sample05
+{
+    public class Comment
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 文章id
+        /// </summary>
+        public int content_id { get; set; }
+        /// <summary>
+        /// 评论内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime add_time { get; set; } = DateTime.Now;
+    }
+}
